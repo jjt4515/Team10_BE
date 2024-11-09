26948ac327912008d45e6abde18cb078df9432c4
stopmin <geemin0@naver.com>
2024-11-09 16:24:20 +0900

    test: FarmScheduleServiceTest - 농장 스케쥴 추가 #125

---
+++ src/test/java/poomasi/domain/farm/_schedule/service/FarmScheduleServiceTest.java
@@ -0,0 +1,91 @@
+package poomasi.domain.farm._schedule.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import poomasi.domain.farm._schedule.dto.FarmScheduleUpdateRequest;
+import poomasi.domain.farm._schedule.entity.FarmSchedule;
+import poomasi.domain.farm._schedule.repository.FarmScheduleRepository;
+import poomasi.global.error.BusinessException;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static poomasi.global.error.BusinessError.FARM_SCHEDULE_ALREADY_EXISTS;
+import static poomasi.global.error.BusinessError.START_TIME_SHOULD_BE_BEFORE_END_TIME;
+
+@ExtendWith(MockitoExtension.class)
+class FarmScheduleServiceTest {
+    @InjectMocks
+    private FarmScheduleService farmScheduleService;
+
+    @Mock
+    private FarmScheduleRepository farmScheduleRepository;
+
+    @Nested
+    @DisplayName("농장 스케줄 추가")
+    class AddFarmSchedule {
+        @Test
+        @DisplayName("정상적으로 스케줄을 추가한다")
+        void should_addFarmSchedule() {
+            // given
+            FarmScheduleUpdateRequest request = new FarmScheduleUpdateRequest(1L, LocalDate.now(), LocalTime.of(10, 0), LocalTime.of(12, 0));
+
+            given(farmScheduleRepository.findByFarmIdAndDate(1L, LocalDate.now())).willReturn(List.of());
+
+            // when
+            farmScheduleService.addFarmSchedule(request);
+
+            // then
+            FarmSchedule farmSchedule = FarmSchedule.builder()
+                    .farmId(1L)
+                    .date(LocalDate.now())
+                    .startTime(LocalTime.of(10, 0))
+                    .endTime(LocalTime.of(12, 0))
+                    .build();
+            assertAll(
+                    () -> assertEquals(1L, farmSchedule.getFarmId()),
+                    () -> assertEquals(LocalDate.now(), farmSchedule.getDate()),
+                    () -> assertEquals(LocalTime.of(10, 0), farmSchedule.getStartTime()),
+                    () -> assertEquals(LocalTime.of(12, 0), farmSchedule.getEndTime())
+            );
+        }
+
+        @Test
+        @DisplayName("시작 시간이 종료 시간보다 늦은 경우 예외를 발생시킨다")
+        void should_throwException_when_startTimeIsAfterEndTime() {
+            // given
+            FarmScheduleUpdateRequest request = new FarmScheduleUpdateRequest(1L, LocalDate.now(), LocalTime.of(10, 0), LocalTime.of(9, 0));
+
+            // when & then
+            BusinessException exception = assertThrows(BusinessException.class, () -> farmScheduleService.addFarmSchedule(request));
+            assertEquals(START_TIME_SHOULD_BE_BEFORE_END_TIME, exception.getBusinessError());
+        }
+
+        @Test
+        @DisplayName("이미 등록된 스케줄이 있는 경우 예외를 발생시킨다")
+        void should_throwException_when_scheduleAlreadyExists() {
+            // given
+            FarmSchedule farmSchedule = FarmSchedule.builder()
+                    .startTime(LocalTime.of(10, 0))
+                    .endTime(LocalTime.of(12, 0))
+                    .build();
+            List<FarmSchedule> farmSchedules = List.of(farmSchedule);
+
+            given(farmScheduleRepository.findByFarmIdAndDate(1L, LocalDate.now())).willReturn(farmSchedules);
+
+            FarmScheduleUpdateRequest request = new FarmScheduleUpdateRequest(1L, LocalDate.now(), LocalTime.of(11, 0), LocalTime.of(13, 0));
+
+            // when & then
+            BusinessException exception = assertThrows(BusinessException.class, () -> farmScheduleService.addFarmSchedule(request));
+            assertEquals(FARM_SCHEDULE_ALREADY_EXISTS, exception.getBusinessError());
+        }
+    }
+}
