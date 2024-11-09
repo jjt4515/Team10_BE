99deee95a2fcbd01a8b2d8e72df59f7a1a2b7456
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-11-09 17:48:06 +0900

    refactor: reissueToken 서비스 로직 인자 수정 #124

--- src/main/java/poomasi/domain/auth/token/reissue/controller/ReissueTokenController.java
+++ src/main/java/poomasi/domain/auth/token/reissue/controller/ReissueTokenController.java
@@ -10,24 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 import poomasi.domain.auth.token.reissue.dto.ReissueRequest;
 import poomasi.domain.auth.token.reissue.dto.ReissueResponse;
 import poomasi.domain.auth.token.reissue.service.ReissueTokenService;
-import poomasi.domain.auth.token.util.JwtUtil;
 
 @RestController
 @RequiredArgsConstructor
 public class ReissueTokenController {
 
     private final ReissueTokenService reissueTokenService;
-    private final JwtUtil jwtUtil;
 
     @PostMapping("/api/reissue")
     public ResponseEntity<ReissueResponse> reissue(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
                                                    @RequestBody ReissueRequest reissueRequest){
 
-        String token = authorizationHeader.replace("Bearer ", "");
+        String accessToken = authorizationHeader.replace("Bearer ", "");
 
-        Long memberId = jwtUtil.getIdFromToken(token);
-
-        return ResponseEntity.ok(reissueTokenService.reissueToken(memberId, reissueRequest));
+        return ResponseEntity.ok(reissueTokenService.reissueToken(accessToken, reissueRequest));
     }
 
 }
