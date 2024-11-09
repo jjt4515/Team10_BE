44442b678d51dcdc1a28ffffb3047caa341dc2a6
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-11-09 16:31:53 +0900

    refactor: 토큰 재발급 액세스 토큰 추출 로직 수정 #124

--- src/main/java/poomasi/domain/auth/token/reissue/controller/ReissueTokenController.java
+++ src/main/java/poomasi/domain/auth/token/reissue/controller/ReissueTokenController.java
@@ -1,30 +1,33 @@
 package poomasi.domain.auth.token.reissue.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import poomasi.domain.auth.security.userdetail.UserDetailsImpl;
 import poomasi.domain.auth.token.reissue.dto.ReissueRequest;
 import poomasi.domain.auth.token.reissue.dto.ReissueResponse;
 import poomasi.domain.auth.token.reissue.service.ReissueTokenService;
-import poomasi.domain.member.entity.Member;
+import poomasi.domain.auth.token.util.JwtUtil;
 
 @RestController
+@RequiredArgsConstructor
 public class ReissueTokenController {
 
-    @Autowired
-    private ReissueTokenService reissueTokenService;
+    private final ReissueTokenService reissueTokenService;
+    private final JwtUtil jwtUtil;
 
-    @Secured({"ROLE_FARMER", "ROLE_CUSTOMER"})
     @PostMapping("/api/reissue")
-    public ResponseEntity<ReissueResponse> reissue(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody ReissueRequest reissueRequest){
-        Member member = userDetails.getMember();
-        return ResponseEntity.ok(reissueTokenService.reissueToken(member.getId(), reissueRequest));
+    public ResponseEntity<ReissueResponse> reissue(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
+                                                   @RequestBody ReissueRequest reissueRequest){
+
+        String token = authorizationHeader.replace("Bearer ", "");
+
+        Long memberId = jwtUtil.getIdFromToken(token);
+
+        return ResponseEntity.ok(reissueTokenService.reissueToken(memberId, reissueRequest));
     }
 
 }
