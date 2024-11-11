1d355f6bd63b6fe287975ded367935551f091ed5
amm0124 <amm012450@gmail.com>
2024-11-11 22:41:48 +0900

    refactoring : 추상 클래스 삭제 및 enum type 변경

--- src/main/java/poomasi/domain/order/_aftersales/dto/FullRefundRequest.java
+++
@@ -1,6 +0,0 @@
-package poomasi.domain.order._aftersales.dto;
-
-public record FullRefundRequest(
-        String refundReason
-) {
-}
--- src/main/java/poomasi/domain/order/entity/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package poomasi.domain.order.entity;
-
-public enum OrderStatus {
-    PENDING,      // 결제 대기 중
-    AWAITING_SELLER_CONFIRMATION, // 판매자 확인 대기 중
-    SELLER_CONFIRMED             // 판매자 확인 완료
-    ;
-}
