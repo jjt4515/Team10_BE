10c2780b8a490c8baba39897c8467d63a0d24b2f
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-11-13 15:24:57 +0900

    feat: s3PresignedUrlPutRequest 검증 추가 #77

--- src/main/java/poomasi/global/config/s3/dto/request/PresignedUrlPutRequest.java
+++ src/main/java/poomasi/global/config/s3/dto/request/PresignedUrlPutRequest.java
@@ -1,6 +1,12 @@
 package poomasi.global.config.s3.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.Map;
 
-public record PresignedUrlPutRequest(String keyPrefix, Map<String, String> metadata) {
+public record PresignedUrlPutRequest(
+        @NotBlank(message = "키 접두어를 입력해주세요")
+        String keyPrefix,
+
+        Map<String, String> metadata) {
 }
