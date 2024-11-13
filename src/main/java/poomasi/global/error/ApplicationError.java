b5fd6d2372e9c30194bc02677407a74b02d84974
stopmin <geemin0@naver.com>
2024-11-14 07:31:43 +0900

    feat: 지원하지 않는 OCR 플랫폼 대응 #136

--- src/main/java/poomasi/global/error/ApplicationError.java
+++ src/main/java/poomasi/global/error/ApplicationError.java
@@ -13,7 +13,10 @@ public enum ApplicationError {
     PAYMENT_NOT_FOUND("결제를 찾을 수 없습니다."),
     PAYMENT_AMOUNT_MISMATCH("사전 결제 금액과 사후 결제 금액이 일치하지 않습니다."),
     PAYMENT_BAD_REQUEST("잘못 된 결제 요청입니다."),
-    PAYMENT_CHECKSUM_EXCESSIVE_REFUND_AMOUNT("환불 요청 금액이 환불 가능한 금액보다 더 많습니다");
+    PAYMENT_CHECKSUM_EXCESSIVE_REFUND_AMOUNT("환불 요청 금액이 환불 가능한 금액보다 더 많습니다"),
+
+    // OCR
+    OCR_SUPPORT_ERROR("지원하지 않는 OCR 서비스입니다.");
 
 
     private final String message;
