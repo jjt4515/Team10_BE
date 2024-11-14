260fa9cdc0d9a6c0f2e9d29ca2f43383a104be01
stopmin <geemin0@naver.com>
2024-11-14 23:57:26 +0900

    feat: 위시리스트 API security 적용 #121

--- src/main/java/poomasi/domain/wishlist/controller/WishListPlatformController.java
+++ src/main/java/poomasi/domain/wishlist/controller/WishListPlatformController.java
@@ -2,32 +2,41 @@ package poomasi.domain.wishlist.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import poomasi.domain.auth.security.userdetail.UserDetailsImpl;
 import poomasi.domain.wishlist.dto.WishListDeleteRequest;
 import poomasi.domain.wishlist.dto.request.WishListAddRequest;
 import poomasi.domain.wishlist.service.WishListPlatformService;
 
-@RequestMapping("/api/v1/wish-list")
+@RequestMapping("/api/v1/wishlist")
 @RestController
 @RequiredArgsConstructor
 public class WishListPlatformController {
     private final WishListPlatformService wishListPlatformService;
 
     @PostMapping("/add")
-    public ResponseEntity<?> addWishList(@RequestBody WishListAddRequest request) {
-        wishListPlatformService.addWishList(request);
+    @Secured("ROLE_CUSTOMER")
+    public ResponseEntity<?> addWishList(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody WishListAddRequest request) {
+        wishListPlatformService.addWishList(userDetails.getMember(), request);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/delete")
-    public ResponseEntity<?> deleteWishList(@RequestBody WishListDeleteRequest request) {
-        wishListPlatformService.deleteWishList(request);
+    @DeleteMapping("/delete")
+    @Secured("ROLE_CUSTOMER")
+    public ResponseEntity<?> deleteWishList(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody WishListDeleteRequest request) {
+        wishListPlatformService.deleteWishList(userDetails.getMember(), request);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/find")
-    public ResponseEntity<?> findWishListByMemberId(@RequestBody Long memberId) {
-        // FIXME : memberID는 SecurityContextHolder에서 가져오도록 수정
-        return ResponseEntity.ok(wishListPlatformService.findWishListByMemberId(memberId));
+    @GetMapping
+    @Secured("ROLE_CUSTOMER")
+    public ResponseEntity<?> findWishListByMemberId(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok(wishListPlatformService.findWishListByMemberId(userDetails.getMember().getId()));
     }
 }
--- src/main/java/poomasi/domain/wishlist/service/WishListPlatformService.java
+++ src/main/java/poomasi/domain/wishlist/service/WishListPlatformService.java
@@ -3,6 +3,7 @@ package poomasi.domain.wishlist.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import poomasi.domain.member.entity.Member;
 import poomasi.domain.wishlist.dto.WishListDeleteRequest;
 import poomasi.domain.wishlist.dto.WishListResponse;
 import poomasi.domain.wishlist.dto.request.WishListAddRequest;
@@ -16,12 +17,12 @@ public class WishListPlatformService {
     private final WishListService wishListService;
 
     @Transactional
-    public void addWishList(WishListAddRequest request) {
+    public void addWishList(Member member, WishListAddRequest request) {
         wishListService.addWishList(request);
     }
 
     @Transactional
-    public void deleteWishList(WishListDeleteRequest request) {
+    public void deleteWishList(Member member, WishListDeleteRequest request) {
         wishListService.deleteWishList(request);
     }
 
