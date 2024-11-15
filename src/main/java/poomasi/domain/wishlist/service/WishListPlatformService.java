ae1800c57116b14b9d0e4556a8e286b482cd95d1
stopmin <geemin0@naver.com>
2024-11-15 12:17:20 +0900

    feat: 위시리스트 조회 API 수정 - 상품 타입 추가 #207

--- src/main/java/poomasi/domain/wishlist/service/WishListPlatformService.java
+++ src/main/java/poomasi/domain/wishlist/service/WishListPlatformService.java
@@ -3,11 +3,15 @@ package poomasi.domain.wishlist.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import poomasi.domain.farm.service.FarmService;
 import poomasi.domain.member.entity.Member;
+import poomasi.domain.product.service.ProductService;
 import poomasi.domain.wishlist.dto.WishListDeleteRequest;
 import poomasi.domain.wishlist.dto.WishListResponse;
 import poomasi.domain.wishlist.dto.request.WishListAddRequest;
 import poomasi.global.common.ServiceType;
+import poomasi.global.error.ApplicationError;
+import poomasi.global.error.ApplicationException;
 
 import java.util.List;
 
@@ -15,6 +19,8 @@ import java.util.List;
 @Service
 public class WishListPlatformService {
     private final WishListService wishListService;
+    private final ProductService productService;
+    private final FarmService farmService;
 
     @Transactional
     public void addWishList(Member member, WishListAddRequest request) {
@@ -28,8 +34,17 @@ public class WishListPlatformService {
 
     @Transactional(readOnly = true)
     public List<WishListResponse> findWishListByMemberIdAndServiceType(Long memberId, ServiceType type) {
-        return wishListService.findWishListByMemberIdAndServiceType(memberId, type).stream()
-                .map(WishListResponse::fromEntity)
-                .toList();
+        if (type.equals(ServiceType.FARM)) {
+            return wishListService.findWishListByMemberId(memberId).stream()
+                    .map(wishList -> WishListResponse.fromEntity(wishList, productService.findProductById(wishList.getObjectId()).getPrice(), productService.findProductById(wishList.getObjectId()).getImages().getFirst().getImageUrl(), productService.findProductById(wishList.getObjectId()).getDescription()))
+                    .toList();
+
+        } else if (type.equals(ServiceType.PRODUCT)) {
+            return wishListService.findWishListByMemberId(memberId).stream()
+                    .map(wishList -> WishListResponse.fromEntity(wishList, farmService.getFarmByFarmId(wishList.getObjectId()).getExperiencePrice(), farmService.getFarmByFarmId(wishList.getObjectId()).getMainImage(), farmService.getFarmByFarmId(wishList.getObjectId()).getDescription()))
+                    .toList();
+        }
+
+        throw new ApplicationException(ApplicationError.ENUM_TYPE_ERROR);
     }
 }
